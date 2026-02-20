23bc1e2b241e87e9af441a86ed177aaef94b2de3
Ondřej Hruboš <hruboson@gmail.com>
2026-02-20 20:59:03 +0100

    feat(robot): basic animation states

--- nodes/actors/elevator/elevator.gd
+++ nodes/actors/elevator/elevator.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 ########## EXPORTS ###########
-@export var leads_to: PackedScene #<--- think about this
+@export var leads_to: PackedScene
 
 var player_inside: bool = false
 
@@ -15,17 +15,21 @@ func _process(delta):
 func open_doors() -> void:
 	pass
 	
+#####
+# @func close_doors
+# @param leads_to_direct: PackedScene (optional)
+#		Overrides the default leads_to variable. This parameter is only used in menu so far.
 func close_doors(leads_to_direct: PackedScene) -> void:
-	var tween = get_tree().create_tween()
+	var animation = get_tree().create_tween()
 	
 	# object, property, value, time
-	tween.tween_property($LeftDoor, "position:x", $LeftDoor.position.x + 50, 2.5)
-	tween.parallel().tween_property($RightDoor, "position:x", $RightDoor.position.x - 50, 2.5)
+	animation.tween_property($LeftDoor, "position:x", $LeftDoor.position.x + 50, 0) # 2.5
+	animation.parallel().tween_property($RightDoor, "position:x", $RightDoor.position.x - 50, 0) # 2.5
 	
 	if(leads_to_direct):
-		tween.tween_callback(transition_to.bind(leads_to_direct))
+		animation.tween_callback(transition_to.bind(leads_to_direct))
 	else:
-		tween.tween_callback(transition_to.bind(leads_to))
+		animation.tween_callback(transition_to.bind(leads_to))
 	
 func transition_to(leads_to: PackedScene) -> void:
 	get_tree().change_scene_to_packed(leads_to)
--- nodes/actors/robot/robot.gd
+++ nodes/actors/robot/robot.gd
@@ -1,20 +1,44 @@
 extends CharacterBody2D
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite
+
+##########################
+# 		CONSTANTS        #
+##########################
 
 const SPEED: float = 300.0
 const JUMP_VELOCITY: float = -400.0
+enum State { IDLE, WALK_LEFT, WALK_RIGHT, INTERACT }
 
-func get_input():
-	if Input.is_action_pressed("left"):
-		velocity.x = -SPEED
-	elif Input.is_action_pressed("right"):
-		velocity.x =  SPEED
-	else:
-		velocity.x = 0
+###########################
+# 		PROPERTIES		  #
+###########################
+
+var current_state = State.IDLE
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor(): # gravity
 		velocity += get_gravity() * delta
 
-	get_input()
+	var state = get_input()
 	move_and_slide()
+	update_animation(state)
+
+func get_input() -> State:
+	if Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+		return State.WALK_LEFT
+	elif Input.is_action_pressed("right"):
+		velocity.x =  SPEED
+		return State.WALK_LEFT
+	else:
+		velocity.x = 0
+		return State.IDLE
+		
+func update_animation(state: State):	
+	if state == State.WALK_LEFT:
+		sprite.play("walk_forward")
+	elif state == State.WALK_RIGHT:
+		sprite.play("walk_forward")
+	else:
+		sprite.play("idle")
