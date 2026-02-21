637b663629edf215e63dac77cc5deece04e991e7
Ondřej Hruboš <hruboson@gmail.com>
2026-02-21 22:48:11 +0100

    feat(levels): strcuture

--- game_state.gd
+++ game_state.gd
@@ -1,6 +1,22 @@
 extends Node
 
 var level1 = {
-	"questLandlord": false,
-	"quest1A": false,
+	# 1 room
+	"questLandlord": false, # update
+	"quest1A": false, # history erase
+}
+
+var level2 = {
+	# 1 rooms + hallway quest
+	"questLandlord1": false, # light
+	"questLandlord2": false, # tubes
+	"quest2A": false, # letter
+	"quest2B": false, # letter back
+}
+
+var level3 = {
+	# 1 room
+	"questLandlord1": false, # breaker
+	"questLandlord2": false, # update
+	"questLandlord3": false, # history erase
 }
