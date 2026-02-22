4161fda037a0a9ce071c1e343096760c58493bb4
Ondřej Hruboš <hruboson@gmail.com>
2026-02-22 09:30:22 +0100

    fix(letters): does not triggle again automatically

--- nodes/actors/door/door_deliver_to.gd
+++ nodes/actors/door/door_deliver_to.gd
@@ -11,7 +11,7 @@ extends Area2D
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D  # must point to node
 
 var player: Robot = null
-var quest_active: int = 0
+var quest_completed: bool = false  # Add this flag to prevent re-triggering
 var enabled = true
 
 func _ready() -> void:
@@ -19,50 +19,49 @@ func _ready() -> void:
 		audio_player.stream = audio_stream  # assign exported stream
 
 func _process(delta: float) -> void:
-	if !enabled:
+	if !enabled or quest_completed:  # Don't process if quest is already completed
 		return 
 		
-	# jesus christ I'm tired...
-	if GameState.letter1Active:
-		if !GameState.levels_state["level2"]["quest2A"]:
-			quest_active = 1
+	# Check if the quest should be active based on GameState
+	if waiting_for_quest == 1:
+		if GameState.letter1Active and !GameState.levels_state["level2"]["quest2A"]:
+			# Quest is active
+			pass
 		else:
-			quest_active = 0
-	elif GameState.letter2Active:
-		if !GameState.levels_state["level2"]["quest2B"]:
-			quest_active = 2
+			return  # Quest not active
+	elif waiting_for_quest == 2:
+		if GameState.letter2Active and !GameState.levels_state["level2"]["quest2B"]:
+			# Quest is active
+			pass
 		else:
-			quest_active = 0
-		
+			return  # Quest not active
+			
 	if player != null and Input.is_action_just_pressed("interact"):
-		if player.input_enabled and quest_active == waiting_for_quest:
-			# holy mother smoking barrel of gun
-			if quest_active == 1:
+		if player.input_enabled:
+			# Complete the quest
+			if waiting_for_quest == 1:
 				GameState.letter1Active = false
 				GameState.levels_state["level2"]["quest2A"] = true
-			if quest_active == 2:
-				GameState.letter2Active = false
-				GameState.levels_state["level2"]["quest2B"] = true
+				quest_completed = true  # Mark as completed
 				
-			player.show_text(subtitles, subtitle_len)
-			audio_player.play()
-			await audio_player.finished
-			
-			# motherfucking jesus, get me out of here
-			# what a fucking hacks, please kill me now
-			if quest_active == 1:
-				quest_active = 2
+				# Trigger next quest
 				GameState.letter2Active = true
 				enable.enabled = true
 				enable.position.x += 200
 				self.position.x += 1000
-			elif quest_active == 2:
+				
+			elif waiting_for_quest == 2:
+				GameState.letter2Active = false
+				GameState.levels_state["level2"]["quest2B"] = true
+				quest_completed = true  # Mark as completed
+				
 				deactivate.enabled = false
 				deactivate.position.x -= 1000
 				self.position.x -= 1000
-				player.show_text("")
-				quest_active = 0
-
+			
+			# Show feedback
+			player.show_text(subtitles, subtitle_len)
+			audio_player.play()
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
