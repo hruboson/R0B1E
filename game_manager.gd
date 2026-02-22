f5f12911f371951cc4f8f70e8c8e30f3681d5d5c
Ondřej Hruboš <hruboson@gmail.com>
2026-02-22 14:31:27 +0100

    fix(player): max energy

--- game_manager.gd
+++ game_manager.gd
@@ -4,7 +4,7 @@ var initialSequenceCompleted = false
 var previous_scene_path: String = ""
 var previous_room_scene_path: String = ""
 var player_return_position: Vector2 = Vector2.ZERO
-var player_energy: int = 5
+var player_energy: int = 15
 
 func take_energy(amount: int) -> void:
 	player_energy = max(player_energy - amount, 0)
